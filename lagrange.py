9a932fa3b0b93ed51f6725bbb8074194239b5612
Hugo Saavedra <affinespaces@unm.edu>
2025-11-15 10:23:19 -0700

    Simplify the Lagrange module

--- lagrange.py
+++ lagrange.py
@@ -1,130 +1,74 @@
+from dataclasses import dataclass, field
+from typing import List
+
 import numpy as np
 
-from functools import cache, cached_property
-from scipy.interpolate import lagrange
-
+@dataclass
 class LagrangePolynomial:
-
-    def __init__(self, nodes, values):
-        self.nodes = np.array(nodes)
-        self.values = list(values)
-
-    @cached_property
-    def indices(self):
-        return np.arange(len(self.nodes))
-
-
-    @cached_property
-    def polynomial(self):
-        applied = [self.L(node) for node in self.nodes]
-        return lagrange(self.nodes, applied)
-
-
-    def numerator_piecemeal(self, i, z):
-        prod = 1
-        for idx in self.indices:
-            if idx == i:
-                continue
-            prod *= (z - self.nodes[idx])
-        return prod
-
-    def denominator_piecemeal(self, i):
-        prod = 1
-        xi = self.nodes[i]
-        for idx in self.indices:
-            if idx == i:
-                continue
-            prod *= xi - self.nodes[idx]
-        return prod
-
-    def l_piecemeal(self, i, z):
-        return self.numerator_piecemeal(i, z) / self.denominator_piecemeal(i)
-
-
-    def l_fast(self, i, z):
-        numerator = 1
-        denominator = 1
-        xi = self.nodes[i]
-        for idx in self.indices:
-            if idx == i:
-                continue
-            numerator *= (z - self.nodes[idx])
-            denominator *= (xi - z)
-        return numerator/denominator
-
-
-    def L_piecemeal(self, z):
-        vals = []
-        for i, x in enumerate(self.nodes):
-            vals.append(self.values[i]*self.l_piecemeal(i, z))
-        return vals
-
-
-    def numerator(self, i, z, j=None):
-        where = self.indices != i
-        return np.prod(z - self.nodes, where=where)
-
-
-    def numerator_special(self, i, j, z):
-        where = (self.indices != i) & (self.indices != j)
-        return np.prod(np.abs(z - self.nodes)**2, where=where)
-
-
-    def denominator_differences(self, i):
-        xi = self.nodes[i]
-        return xi - self.nodes[self.indices != i]
-
-
-    def numerator_differences(self, i, z):
-        return z - self.nodes[self.indices != i]
-
-
-    @cache
-    def denominator(self, i):
-        xi = self.nodes[i]
-        return np.prod(xi - self.nodes, where=self.indices != i)
-
-
-    def numerators(self, z):
-        return [self.numerator(i, z) for i, _ in enumerate(self.nodes)]
-
+    _nodes: list = field(default_factory=list)
+    _values: list = field(default_factory=list)
 
     @property
-    def denominators(self):
-        return [self.denominator(i) for i, _ in enumerate(self.nodes)]
+    def nodes(self):
+        if not isinstance(self._nodes, np.ndarray):
+            return np.array(self._nodes)
+        return self._nodes
+    
+
+    @property
+    def values(self):
+        if not isinstance(self._values, np.ndarray):
+            return np.array(self._values)
+        return self._values
+    
+
+def ell(L: LagrangePolynomial):
+    return np.poly1d(L.nodes, r=True)
 
 
-    def l(self, i, z):
-        return self.numerator(i, z)/self.denominator(i)
+def barycentric_weights(L: LagrangePolynomial):
+    weights = []
+    for k in range(L.nodes.size):
+        prod = 1
+        for j in range(L.nodes.size):
+            if k == j:
+                continue
+            prod *= 1/(L.nodes[k] - L.nodes[j])
+        weights.append(prod)
+    return np.array(weights)
 
 
-    def lp(self, i):
-        values = self.l_piecemeal(i, self.nodes)
-        return lagrange(self.nodes, values)
+def displacement(L: LagrangePolynomial, z: complex):
+    return barycentric_weights(L)/(z - L.nodes)
 
 
-    def lv(self, z):
-        num = np.array(self.numerators(z))
-        return num/np.array(self.denominators)
+def evaluate(L: LagrangePolynomial, z: complex):
+    d = displacement(L, z)
+    return ell(L)(z)*(d@L.values)
 
 
-    def L(self, z):
-        return self.L_seq(z).sum()
+def square_modulus(poly: LagrangePolynomial, z: complex):
+    l = ell(L)
+    d = displacement(L, z)
+    terms = d*L.values
+    first_factor = (l(z)*l(z).conjugate())
+    second_factor = np.array([term*terms.conjugate() for term in terms])
+    return first_factor*second_factor
 
 
-    def group(self, grouping, zv):
-        groupings = []
-        for index_set in grouping:
-            grouping = []
-            for i in index_set:
-                groupings.append(
-                    self.values[i]*self.l
-                )
+def sequence_sqmod(left, right):
+    return np.array([t*right.conjugate() for t in left])
 
 
-    def L_seq(self, z):
-        return np.array(self.values)*self.lv(z)
+def square_modulus(poly: LagrangePolynomial, z: complex):
+    l = ell(L)
+    d = displacement(L, z)
+    terms = d*L.values
+    first_factor = (l(z)*l(z).conjugate())
+    second_factor = sequence_sqmod(terms, terms)
+    return first_factor*second_factor
 
 
-    def __call__(self, z):
-        return np.vectorize(self.L_piecemeal)(z)
+def square_modulus_1d(poly: LagrangePolynomial, z: complex):
+    return sum(square_modulus(poly, z))
+        
